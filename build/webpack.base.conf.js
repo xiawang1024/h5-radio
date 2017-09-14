cb954a862e0ff987cba24b3d2f80c1489a870b4c
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-09-14 15:58:48 +0800

    压缩图片

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -42,7 +42,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 300000,
+          limit: 500000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
